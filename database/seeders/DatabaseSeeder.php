63b299121f72455247cfd33928219d79b8a277c6
onrsnos <onrsnos@gmail.com>
2024-10-23 18:57:18 +0300

    seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Crypt;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\GorevlerSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,47 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        // User::insert([
-        //     [
-        //         'name' => 'Test User',
-        //         'email' => 'test11@example.com',
-        //         'password' => Crypt::encryptString('12345')
-        //     ],
-        //     [
-        //         'name' => 'Test 2 User',
-        //         'email' => 'test2@example.com',
-        //         'password' => Crypt::encryptString('12345')
+        $this->call([
 
-        //     ],
-        //     [
-        //         'name' => 'Test 3 User',
-        //         'email' => 'test3@example.com',
-        //         'password' => Crypt::encryptString('12345')
-
-        //     ], 
-        //     [
-        //         'name' => 'Test 4 User',
-        //         'email' => 'test4@example.com',
-        //         'password' => Crypt::encryptString('12345')
-
-        //     ],
-        //     [
-        //         'name' => 'Test 5 User',
-        //         'email' => 'test5@example.com',
-        //         'password' => Crypt::encryptString('12345')
-
-        //     ], 
-        //     [
-        //         'name' => 'Test 6 User',
-        //         'email' => 'test6@example.com',
-        //         'password' => Crypt::encryptString('12345')
-
-        //     ],
-
-
-        // ]);
+            UserSeeder::class,
+            GorevlerSeeder::class,
+            
+        ]);
+        
         
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Crypt;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        User::insert([
+            [
+                'name' => 'Test User',
+                'email' => 'test11@example.com',
+                'password' => Crypt::encryptString('12345')
+            ],
+            [
+                'name' => 'Test 2 User',
+                'email' => 'test2@example.com',
+                'password' => Crypt::encryptString('12345')
+
+            ],
+            [
+                'name' => 'Test 3 User',
+                'email' => 'test3@example.com',
+                'password' => Crypt::encryptString('12345')
+
+            ], 
+            [
+                'name' => 'Test 4 User',
+                'email' => 'test4@example.com',
+                'password' => Crypt::encryptString('12345')
+
+            ],
+            [
+                'name' => 'Test 5 User',
+                'email' => 'test5@example.com',
+                'password' => Crypt::encryptString('12345')
+
+            ], 
+            [
+                'name' => 'Test 6 User',
+                'email' => 'test6@example.com',
+                'password' => Crypt::encryptString('12345')
+
+            ],
+
+
+        ]);
+    }
+}
